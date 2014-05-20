5af2e647441e40ea17d084d96b50b2e08eda82e2
tfiers <tomas.fiers@gmail.com>
2014-05-20 17:07:24 +0200

    Methode 'execute(Program)' aan Statement interface toegevoegd, if-statement als voorbeeld geïmplementeerd en abstracte klasse 'ActionStatement' toegevoegd.
    Nog geen testen. (Niet voor de hand liggend hoe te testen of een statement uitgevoerd werd.)

--- src/worms/model/programs/statements/Statement.java
+++ src/worms/model/programs/statements/Statement.java
@@ -4,5 +4,5 @@ import worms.model.Program;
 import worms.model.programs.Executable;
 
 public interface Statement extends Executable{
-	void execute(Program program);
+	public void execute(Program program);
 }
