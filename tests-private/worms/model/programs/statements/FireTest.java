f981c7ef02f58ac443ba9a7fd2b4856673a55a17
Jens Claes <jensclaes33@gmail.com>
2014-05-22 17:26:39 +0200

    Added ActionHandlerMock. FIxed FireTest

--- tests-private/worms/model/programs/statements/FireTest.java
+++ tests-private/worms/model/programs/statements/FireTest.java
@@ -7,11 +7,9 @@ import java.util.Random;
 import org.junit.Before;
 import org.junit.Test;
 
-import worms.gui.game.IActionHandler;
-import worms.model.Facade;
+import worms.model.ActionHandlerMock;
 import worms.model.Program;
 import worms.model.ProgramMock;
-import worms.model.SimpleActionHandler;
 import worms.model.World;
 import worms.model.Worm;
 import worms.model.programs.WormsRuntimeException;
@@ -22,10 +20,11 @@ public class FireTest {
 	ActionStatement fire;
 	Program program;
 	Worm willy;
+	ActionHandlerMock actionHandler;
 
 	@Before
 	public void setUp() throws Exception {
-		IActionHandler actionHandler = new SimpleActionHandler(new Facade());
+		actionHandler = new ActionHandlerMock();
 		program = new ProgramMock(null, null, actionHandler);
 		World world = new World(20,30,new boolean[][]{{true,true},{false,true},{true,true}},new Random());
 		willy  = new Worm(world, 112, 358, 1.321, 34.55, "Willy Wonka", null, program);
@@ -65,7 +64,7 @@ public class FireTest {
 		System.out.println(willy.getSelectedWeapon().getCost());
 		assertTrue(willy.canFire(80));
 		fire.execute(program);
-		assertEquals(AP-willy.getSelectedWeapon().getCost(), willy.getActionPoints());
+		assertTrue(actionHandler.hasCalledFire);
 	}
 	
 	//--------------------------------------------------------------------------
