b2f7dff89754057eec091b6942512525f31feb2e
tfiers <tomas.fiers@gmail.com>
2014-05-22 18:03:57 +0200

    Test Foreach. Test needs fix.

--- tests-private/worms/model/programs/statements/ForeachTest.java
+++ tests-private/worms/model/programs/statements/ForeachTest.java
@@ -1,7 +1,9 @@
 package worms.model.programs.statements;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
@@ -10,25 +12,23 @@ import org.junit.Before;
 import org.junit.Test;
 
 import worms.model.Food;
-import worms.model.Program;
 import worms.model.ProgramMock;
 import worms.model.World;
 import worms.model.Worm;
-import worms.model.programs.WormsRuntimeException;
 import worms.model.programs.ProgramFactory.ForeachType;
-import worms.model.programs.expressions.BooleanLiteral;
-import worms.model.programs.expressions.EntityNullLiteral;
-import worms.model.programs.expressions.Expression;
-import worms.model.programs.types.BooleanType;
-import worms.model.programs.types.DoubleType;
+import worms.model.programs.WormsRuntimeException;
+import worms.model.programs.expressions.EntityLiteral;
 import worms.model.programs.types.EntityType;
 import worms.model.programs.types.Type;
 
+//FIXME something with Program/ProgramMock
 public class ForeachTest {
 	
 	Statement foreachWorm, foreachFood, foreachAny;
 	Statement bodySt1, bodySt2, bodySt3;
 	ProgramMock programMock;
+	Worm willy, jef;
+	Food pizza;
 
 	@Before
 	public void setUp() throws Exception {
@@ -45,9 +45,10 @@ public class ForeachTest {
 		programMock = new ProgramMock(null, globals, null);
 		
 		World world = new World(20,30,new boolean[][]{{true,true},{false,true},{true,true}},new Random());
-		Worm willy  = new Worm(world, 112, 358, 1.321, 34.55, "Willy Wonka", null, programMock);
-		Worm jef = new Worm(world, 1, 2, 0, 5, "Jef", null, programMock); // TODO maybe copy prog
-		Food pizza = new Food(world);
+		willy  = new Worm(world, 112, 358, 1.321, 34.55, "Willy Wonka", null, programMock);
+		jef = new Worm(world, 1, 2, 0, 5, "Jef", null, programMock);
+		//programMock = (ProgramMock) jef.getProgram();
+		pizza = new Food(world);
 	}
 
 	@Test(expected = WormsRuntimeException.class)
@@ -62,13 +63,42 @@ public class ForeachTest {
 	}
 	
 	@Test
-	public void testExecute_Worms() {
-		//First run => condition equals true
-		programMock.runStatement(whileStChanging);
-		assertArrayEquals(new Statement[]{bodySt1, whileStChanging}, programMock.getExecutionStackAsArray());
+	public void testExecute_ForeachWorm() {
+		programMock.runStatement(foreachWorm);
+		Statement[] willyStatements = 
+				new Statement[]{new Assignment("e", new EntityLiteral(willy)), bodySt1};
+		Statement[] jefStatements = 
+				new Statement[]{new Assignment("e", new EntityLiteral(jef)), bodySt1};
 		
-		//Second run => condition equals false
-		programMock.runStatement(whileStChanging);
-		assertArrayEquals(new Statement[]{}, programMock.getExecutionStackAsArray());
+		assertTrue(hasAsSubArray(programMock.getExecutionStackAsArray(), willyStatements));
+		assertTrue(hasAsSubArray(programMock.getExecutionStackAsArray(), jefStatements));
+	}
+	
+	@Test
+	public void testExecute_ForeachFood() {
+		programMock.runStatement(foreachFood);
+		Statement[] pizzaStatements = 
+				new Statement[]{new Assignment("e", new EntityLiteral(pizza)), bodySt2};
+		
+		assertTrue(hasAsSubArray(programMock.getExecutionStackAsArray(), pizzaStatements));
+	}
+	
+	@Test
+	public void testExecute_ForeachAny() {
+		programMock.runStatement(foreachAny);
+		Statement[] willyStatements = 
+				new Statement[]{new Assignment("e", new EntityLiteral(willy)), bodySt3};
+		Statement[] jefStatements = 
+				new Statement[]{new Assignment("e", new EntityLiteral(jef)), bodySt3};
+		Statement[] pizzaStatements = 
+				new Statement[]{new Assignment("e", new EntityLiteral(pizza)), bodySt3};
+		
+		assertTrue(hasAsSubArray(programMock.getExecutionStackAsArray(), willyStatements));
+		assertTrue(hasAsSubArray(programMock.getExecutionStackAsArray(), jefStatements));
+		assertTrue(hasAsSubArray(programMock.getExecutionStackAsArray(), pizzaStatements));
+	}
+	
+	private boolean hasAsSubArray(Statement[] array, Statement[] subArray) {
+		return (Collections.indexOfSubList(Arrays.asList(array), Arrays.asList(subArray)) != -1);
 	}
 }
